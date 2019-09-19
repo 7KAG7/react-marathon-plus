75a360c496016c8f2f1c75204ca88ad8c5ba739e
Tedder <25165500+TheTedder@users.noreply.github.com>
2019-09-19 13:41:43 -0400

    show only songs belonging to selected playlist

--- src/components/App.js
+++ src/components/App.js
@@ -3,20 +3,32 @@ import PlaylistCollection from './PlaylistCollection'
 import SongCollection from './SongCollection'
 
 const App = (props) => {
-  
+  let [selectedPlaylistId, setSelectedPlaylistId] = useState(null)
+
+  const selectedPlaylistSongs = props.data.songs.filter(song => {
+    let playlist = props.data.playlists.find(playlist => playlist.id === selectedPlaylistId)
+    if (playlist === undefined) {
+      return false
+    }
+
+    return playlist.songs.includes(song.id)
+  })
+
   return (
     <div className="app row callout">
 
       <h2 className="title">React Music Player</h2>
       <div className="columns small-6 playlists">
         <PlaylistCollection
+          selectedPlaylistId={selectedPlaylistId}
+          setSelectedPlaylistId={setSelectedPlaylistId}
           playlists={props.data.playlists}
         />
 
       </div>
       <div className="columns small-6 songs">
         <SongCollection
-          songs={props.data.songs}
+          songs={selectedPlaylistSongs}
         />
       </div>
     </div>
